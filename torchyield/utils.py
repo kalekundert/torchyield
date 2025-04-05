d30237b7c4761108d859dcbdfd54f846fef30672
Kale Kundert <kale@thekunderts.net>
2025-04-05 15:23:53 -0400

    fix: improve tensor repr

--- torchyield/utils.py
+++ torchyield/utils.py
@@ -1,5 +1,5 @@
 import torch
 
 def monkeypatch_tensor_repr():
-    torch.Tensor.__repr__ = lambda x: f'torch.Tensor(shape={x.shape})'
+    torch.Tensor.__repr__ = lambda x: f'torch.Tensor(shape={tuple(x.shape)})'
 
